1f4832e0d4751b1eb4e75aa518089a294c7edd88
Ruthmisiko <ruthmisiko2@gmail.com>
2024-09-02 04:25:26 -0400

    fixes

--- genre/urls.py
+++ genre/urls.py
@@ -1,7 +1,18 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('hello.html', views.index, name='hello'),
+#     path('contact.html', views.contact, name='contact'),
+#     path('service.html', views.service, name='service'),
+#     path('about.html', views.about, name='about'),
+#     path('guard.html', views.guard, name='guard'),
+# ]
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='genre-home'),  # This will serve hello.html at /genre/
     path('hello.html', views.index, name='hello'),
     path('contact.html', views.contact, name='contact'),
     path('service.html', views.service, name='service'),
